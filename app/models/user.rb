4a2bca2122ec2237aaae459d2ce7ca4be2ae5014
Devin Mandelbaum <email": null>
2016-02-19 16:00:47 -0500

    Add sessions helper, ability to login and sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
-  has_many :surveys, foreign_key: "author_id"#, dependent: :destroy
+  has_many :surveys, foreign_key: "author_id", dependent: :destroy
   has_many :responses, foreign_key: "taker_id"
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
+  validates :password_digest, length: {minimum: 8}
+
+  has_secure_password
 end
