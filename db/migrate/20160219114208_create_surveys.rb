8852b95dd016a0375a96d46d185bf787a032441c
Steven King <stevenking86@gmail.com>
2016-02-19 17:33:03 -0500

    put comment in migration

--- db/migrate/20160219114208_create_surveys.rb
+++ db/migrate/20160219114208_create_surveys.rb
@@ -1,6 +1,7 @@
 class CreateSurveys < ActiveRecord::Migration
   def change
     create_table :surveys do |t|
+      #make titles unique, probably
       t.string :title, null: false
       t.integer :author_id, index: true
 
